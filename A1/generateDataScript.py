247a5a5f90a2a19133e32205234fde42a9f10f45
Andrey Dyblenko <dyblenko@mathlab.utsc.utoronto.ca>
2013-09-25 20:00:41 -0400

    added data script and generated data

---
+++ A1/generateDataScript.py
@@ -0,0 +1,20 @@
+from random import randint
+import glob
+
+#print "hello!"
+id = 0
+f = open("data", "w")
+while id < 5000:
+	a = randint(0,100)
+	b = randint(0,100)
+	c = randint(0,100)
+	line = "%d, %d, %d, %d\n" % (id,a,b,c)
+	id+=1
+	
+	#print "%s" % line
+	f.write(line)
+
+f.close()
+
+	
+	 
