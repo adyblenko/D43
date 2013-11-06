384c9832c58b3a1ad84bb1414652de07d2e2ef7e
Andrey Dyblenko <dyblenko@mathlab.utsc.utoronto.ca>
2013-11-06 12:03:36 -0500

    added bitArray operations, still trying to figure out how the hashvalues work

--- a2/hashFns.c
+++ a2/hashFns.c
@@ -7,17 +7,22 @@
 #define BIT2 0x40
 #define BIT1 0x80
 
-char bits[8] = {BIT1, BIT2, BIT3, BIT4, BIT5, BIT6, BIT7, BIT8};
 
+#include <stdio.h>
+#include <string.h>
+#include <limits.h>
 
-char eightBits = 0;
-//eightBits = eightBits & (1 << 4);
+void printbincharpad(char c)
+{
+//TODO: only used for debugging http://stackoverflow.com/questions/7863499/conversion-of-char-to-binary-in-c
+    int i;
 
-eightBits &= (bits[3] & bits[4]);
-//eightBits now looks like "00110000".
-
-
-//http://eternallyconfuzzled.com/tuts/algorithms/jsw_tut_hashing.aspx
+    for (i = 7; i >= 0; --i)
+    {
+        putchar( (c & (1 << i)) ? '1' : '0' );
+    }
+    putchar('\n');
+}
 
 unsigned fnv_hash ( void *key, int len )
 {
@@ -28,6 +33,71 @@ unsigned fnv_hash ( void *key, int len )
 		h = ( h * 16777619 ) ^ p[i];
 	return h;
 }
+
+
+
+
+int main()
+{
+	//char bits[8] = {BIT1, BIT2, BIT3, BIT4, BIT5, BIT6, BIT7, BIT8};
+
+	char eightBitArray = 0;
+	
+	//TODO: make array bigger so that 0s count? / convert int to binary rep?
+	int data = 44;
+	int len = 2;//strlen(data);
+
+	int hashValue = fnv_hash(&data, len);
+	
+	//TODO: make sure normalized between 1 and 8
+	int normalized = hashValue / ((double)INT_MAX / 7.0) + 1.0;
+	
+	printf("%s %d \n", "normalized", normalized);
+	//insert hash values into the bitArray
+	switch(normalized){
+		case 1: eightBitArray |= BIT1;break;
+		case 2: eightBitArray |= BIT2;break;
+		case 3: eightBitArray |= BIT3;break;
+		case 4: eightBitArray |= BIT4;break;
+		case 5: eightBitArray |= BIT5;break;
+		case 6: eightBitArray |= BIT6;break;
+		case 7: eightBitArray |= BIT7;break;
+		case 8: eightBitArray |= BIT8;break;
+		default: printf("%s %d \n", "Unknown hash value", hashValue);
+	}
+	
+	printbincharpad(eightBitArray);
+	
+	//check if hash value is in array
+	int queryHash = 0;
+	char result;
+
+	switch(queryHash){
+                case 1: result = eightBitArray & BIT1;break;
+                case 2: result = eightBitArray & BIT2;break;
+                case 3: result = eightBitArray & BIT3;break;
+                case 4: result = eightBitArray & BIT4;break;
+                case 5: result = eightBitArray & BIT5;break;
+                case 6: result = eightBitArray & BIT6;break;
+                case 7: result = eightBitArray & BIT7;break;
+                case 8: result = eightBitArray & BIT8;break;
+                default: printf("%s %d \n", "Unknown hash value", queryHash);
+        }
+
+	if (result == 0){
+		printf("%s", "NO match");
+	}else{
+		printf("%s", "match!");
+	}
+
+	printbincharpad(result);
+
+
+}
+
+
+//http://eternallyconfuzzled.com/tuts/algorithms/jsw_tut_hashing.aspx
+
 
 unsigned oat_hash ( void *key, int len )
 {
